88c86e0f59aaacb1dbfe17b1e3373fe36ef7bbda
amolmagdum25 <amolmagdum25@yahoo.in>
2017-07-11 08:58:17 +0530

    Add files via upload

--- src/com/javaPractise/homework/HomeWork_1.java
+++ src/com/javaPractise/homework/HomeWork_1.java
@@ -3,8 +3,33 @@ package com.javaPractise.homework;
 public class HomeWork_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		char gender = 'm';
+		int age = 20;
+		
+		//
+		if(gender == 'f'){
+			
+			if(age > 18){
+				System.out.println("Ready to marry");
+			}
+			
+			else{
+				System.out.println("Not ready to marry");
+			}
+		}
+		
+		if(gender == 'm') {
+			
+			if(age > 21){
+				System.out.println("Ready to marry");
+			}
+			
+			else{
+				System.out.println("Not ready to marry");
+			}
+			
+		}
+				
 	}
-
 }
